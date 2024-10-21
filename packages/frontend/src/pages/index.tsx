b18d5b77eef4e97d19b59d9b10dc917524d59a0b
Marvin Frachet <marvin.frachet@protonmail.com>
2024-10-21 10:45:09 +0200

    chore(frontend): add nivo and line chart

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,7 +1,14 @@
+import { LineChart } from "../components/charts/LineChart";
+import { lineChartFixture } from "../fixtures/lineChart.fixture";
+
 export const IndexPage = () => {
   return (
     <main>
       <h1 className="text-2xl font-bold">Hello world</h1>
+
+      <div className="w-full h-96">
+        <LineChart data={lineChartFixture} />
+      </div>
     </main>
   );
 };
