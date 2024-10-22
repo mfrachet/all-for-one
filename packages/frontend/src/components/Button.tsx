8b7a694e05b37a41b1b8fb6fd388033c5dc673ca
Marvin Frachet <marvin.frachet@protonmail.com>
2024-10-22 08:52:15 +0200

    feat(charts): customize charts (in progress)

--- packages/frontend/src/components/Button.tsx
+++ packages/frontend/src/components/Button.tsx
@@ -6,14 +6,19 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   to?: string;
 }
 
-export const Button = ({ children, icon, ...props }: ButtonProps) => {
+export const Button = ({
+  children,
+  icon,
+  as: Root = "button",
+  ...props
+}: ButtonProps) => {
   return (
-    <button
+    <Root
       className="bg-black rounded-3xl text-white hover:bg-gray-900 active:bg-gray-800 px-4 py-2 flex flex-row items-center gap-2"
       {...props}
     >
       {icon && <div className="[&>svg]:w-4 [&>svg]:h-4">{icon}</div>}
       {children}
-    </button>
+    </Root>
   );
 };
--- packages/frontend/src/components/charts/LineChart/LineChart.tsx
+++ packages/frontend/src/components/charts/LineChart/LineChart.tsx
@@ -14,79 +14,86 @@ export interface LineChartProps {
   data: LineSvgProps["data"];
 }
 
+const CustomLegend = ({ data }: LineChartProps) => {
+  return (
+    <ul className="list-none overflow-x-scroll">
+      {data.map((d) => (
+        <li key={d.id} className="flex flex-row items-center gap-2">
+          <div
+            className="w-2 h-2 rounded-full"
+            style={{ background: d.color }}
+          />
+          {d.id}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 export const LineChart = ({ data }: LineChartProps) => (
-  <ResponsiveLine
-    data={data}
-    lineWidth={1}
-    margin={{ top: 4, right: 30, bottom: LegendOffset + 16, left: 30 }}
-    xScale={{ type: "point" }}
-    yScale={{
-      type: "linear",
-      min: "auto",
-      max: "auto",
-      stacked: false,
-      reverse: false,
-    }}
-    axisTop={null}
-    axisRight={null}
-    axisBottom={{
-      tickSize: AxisTickSize,
-      tickPadding: AxisTickPadding,
-      tickValues: 10,
-    }}
-    axisLeft={null}
-    enableGridX={false}
-    pointSize={0}
-    crosshairType="x"
-    enableSlices="x"
-    enablePointLabel={false}
-    enableArea={false}
-    areaBlendMode="normal"
-    areaBaselineValue={0}
-    isInteractive
-    areaOpacity={0}
-    pointLabel=""
-    colors={(p) => p.color}
-    sliceTooltip={Tooltip}
-    tooltip={() => null}
-    enableGridY={true}
-    enableCrosshair={true}
-    pointColor={{ from: "color", modifiers: [] }}
-    pointBorderColor={"transparent"}
-    pointBorderWidth={0}
-    enablePoints={false}
-    useMesh={true}
-    debugMesh={false}
-    debugSlices={false}
-    legends={[
-      {
-        anchor: "bottom",
-        direction: "row",
-        justify: false,
-        translateX: 0,
-        translateY: LegendOffset,
-        itemsSpacing: 16,
-        itemDirection: "left-to-right",
-        itemWidth: 100,
-        itemHeight: 20,
-        symbolSize: 10,
-        symbolShape: "circle",
-        itemTextColor: LightTextColor,
-      },
-    ]}
-    theme={customizedTheme()}
-    layers={
-      [
-        "grid",
-        "axes",
-        "areas",
-        "lines",
-        "crosshair",
-        "slices",
-        "mesh",
-        "legends",
-        ActivePoint,
-      ] as Array<Layer>
-    }
-  />
+  <div className="relative h-full w-full flex flex-row gap-0">
+    <ResponsiveLine
+      data={data}
+      curve="natural"
+      lineWidth={1}
+      margin={{ top: 4, right: 8, bottom: 40, left: 8 }}
+      xScale={{ type: "point" }}
+      yScale={{
+        type: "linear",
+        min: "auto",
+        max: "auto",
+        stacked: false,
+        reverse: false,
+      }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        tickSize: AxisTickSize,
+        tickPadding: AxisTickPadding,
+        tickValues: 10,
+      }}
+      axisLeft={null}
+      enableGridX={false}
+      pointSize={0}
+      crosshairType="x"
+      enableSlices="x"
+      enablePointLabel={false}
+      enableArea={false}
+      areaBlendMode="normal"
+      areaBaselineValue={0}
+      isInteractive
+      areaOpacity={0}
+      pointLabel=""
+      colors={(p) => p.color}
+      sliceTooltip={Tooltip}
+      tooltip={() => null}
+      enableGridY={true}
+      enableCrosshair={true}
+      pointColor={{ from: "color", modifiers: [] }}
+      pointBorderColor={"transparent"}
+      pointBorderWidth={0}
+      enablePoints={false}
+      useMesh={true}
+      debugMesh={false}
+      debugSlices={false}
+      theme={customizedTheme()}
+      layers={
+        [
+          "grid",
+          "axes",
+          "areas",
+          "lines",
+          "crosshair",
+          "slices",
+          "mesh",
+          "legends",
+          ActivePoint,
+        ] as Array<Layer>
+      }
+    />
+
+    <div className="shrink-0">
+      <CustomLegend data={data} />
+    </div>
+  </div>
 );
