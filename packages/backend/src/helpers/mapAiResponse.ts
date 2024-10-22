2067431c9a4ce46f24afd4bfc2261a7a4e01d54f
Marvin Frachet <marvin.frachet@protonmail.com>
2024-10-22 11:42:52 +0200

    chore(ai): try to make the prompt do some stuff

--- packages/backend/src/helpers/mapAiResponse.ts
+++ packages/backend/src/helpers/mapAiResponse.ts
@@ -4,7 +4,7 @@ import { ExpectedOutput, LineChartOutput } from "../types";
 export const mapAiResponse = (
   response: Array<ExpectedSqlColumns>
 ): ExpectedOutput => {
-  const responseType: ExpectedOutput[number]["type"] = response[0].type;
+  const responseType: ExpectedOutput[number]["type"] = response[0]?.type;
 
   if (!responseType) {
     return [];
