a2d15a6fdb0cf91781e657122bf5477b78d77943
Marvin Frachet <marvin.frachet@protonmail.com>
2024-10-23 16:17:02 +0200

    fake auth

--- packages/frontend/src/components/BigStat.tsx
+++ packages/frontend/src/components/BigStat.tsx
@@ -6,13 +6,13 @@ export interface BigStatProps {
 
 export const BigStat = ({ title, value, color }: BigStatProps) => {
   return (
-    <div className="flex flex-row gap-2 min-w-24">
+    <div className="flex flex-row gap-2 min-w-24 bg-white rounded-lg overflow-hidden border border-gray-100">
       <div
-        className="h-[40px] w-1 rounded-full"
+        className="h-full w-1 rounded-full"
         style={{ backgroundColor: color }}
       ></div>
 
-      <div className="flex flex-col">
+      <div className="flex flex-col px-4 py-2">
         <div className="text-xs text-gray-500">{title}</div>
         <div className="text-xl font-bold">{value}</div>
       </div>
--- packages/frontend/src/components/ChartWrapper.tsx
+++ packages/frontend/src/components/ChartWrapper.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 export const ChartWrapper = ({ children }: { children: ReactNode }) => {
-  const classnames = `overflow-hidden flex h-96 shadow-md rounded-3xl border border-gray-100 bg-white`;
+  const classnames = `overflow-hidden flex h-96 rounded-lg border border-gray-100 bg-white`;
 
   return <div className={classnames}>{children}</div>;
 };
