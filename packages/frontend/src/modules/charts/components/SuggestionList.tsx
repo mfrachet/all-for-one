427b062f9e61816d047d6ecff9a5d9266d07a8fa
Marvin Frachet <marvin.frachet@protonmail.com>
2024-10-23 16:35:36 +0200

    fake auth

--- packages/frontend/src/modules/charts/components/SuggestionList.tsx
+++ packages/frontend/src/modules/charts/components/SuggestionList.tsx
@@ -14,20 +14,25 @@ export const SuggestionList = ({ suggestions, type }: SuggestionListProps) => {
   const { addMessage } = useMessages();
 
   return (
-    <div>
-      <h3 className="text-xs text-gray-500 pb-2">Suggestions</h3>
-      <Suspense
-        fallback={
+    <Suspense
+      fallback={
+        <div>
+          <h3 className="text-xs text-gray-500 pb-2">Suggestions</h3>
           <div className="flex flex-row gap-2 items-center">
             <Skeleton />
             <Skeleton />
             <Skeleton />
           </div>
-        }
-      >
-        <Await resolve={suggestions}>
-          {(list: SuggestionDict) => {
-            return (
+        </div>
+      }
+    >
+      <Await resolve={suggestions}>
+        {(list: SuggestionDict) => {
+          if (list[type].length === 0) return null;
+
+          return (
+            <div>
+              <h3 className="text-xs text-gray-500 pb-2">Suggestions</h3>
               <div className="flex flex-row gap-2 flex-wrap">
                 {list[type].map((suggestion) => (
                   <Tag
@@ -38,10 +43,10 @@ export const SuggestionList = ({ suggestions, type }: SuggestionListProps) => {
                   </Tag>
                 ))}
               </div>
-            );
-          }}
-        </Await>
-      </Suspense>
-    </div>
+            </div>
+          );
+        }}
+      </Await>
+    </Suspense>
   );
 };
