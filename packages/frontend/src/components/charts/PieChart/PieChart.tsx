c97c64a29a6cada08923c9c985afd4befd58e49c
Marvin Frachet <marvin.frachet@protonmail.com>
2024-10-24 08:48:39 +0200

    fake auth

--- packages/frontend/src/components/charts/PieChart/PieChart.tsx
+++ packages/frontend/src/components/charts/PieChart/PieChart.tsx
@@ -13,7 +13,7 @@ export const PieChart = ({ data }: PieChartProps) => {
   return (
     <ResponsivePie
       data={data}
-      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      margin={{ top: 40, right: 80, bottom: 40, left: 80 }}
       innerRadius={0.5}
       padAngle={0.7}
       cornerRadius={3}
