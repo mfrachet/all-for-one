04572ff36595cb42d7168455428465666304953a
Marvin Frachet <marvin.frachet@protonmail.com>
2024-10-24 07:58:27 +0200

    fake auth

--- packages/ai/src/promps/getSuggestions/getSuggestions.prompt.ts
+++ packages/ai/src/promps/getSuggestions/getSuggestions.prompt.ts
@@ -4,6 +4,7 @@ import { expectedOutput } from "./output";
 export const getSuggestionsPrompt = (input: string) => `
     Your scope is limited to the DDL passed in this context: "${tables}".
     You will generate a JSON of type "Suggestions" from the following type definition: "${expectedOutput}", nothing else is authorized.
+    For example, a valid output is: [{"title": "Subscription status count", "type": "paragraph" }] .
     The JSON should be strict and valid, not containing any escaping characters, new lines, markdown or code blocks.
     You will suggest new charts name that don't exist in the input you're provided with.
     When the type is "paragraph", the result of the chart should only be one number, nothing else.
