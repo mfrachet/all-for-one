2e8342368b5066bb7c911d7841f19fd2555ddaf9
Marvin Frachet <marvin.frachet@protonmail.com>
2024-10-24 11:51:51 +0200

    types

--- packages/ai/src/promps/generateClickhouseQuery/output.ts
+++ packages/ai/src/promps/generateClickhouseQuery/output.ts
@@ -43,10 +43,13 @@ export const expectedSqlColumns = `
   For lineChart, the columns should follow this format: {x:date, y:number, groupingKey?:string}. groupingKey should be a name, id, or some identifier.
   For pieChart, the columns should be: {category: string, value: number, groupingKey?:string}. groupingKey should be a name, id, or some identifier.
   For paragraph, the columns should follow this format: {text: string} and should only contain the result of the query, nothing else.
-  For mapChart, the columns should be: {id: string, value: number} where "id" is a string containing a GeoJSON code following the "ISO 3166-1 alpha-3" norm, for example "FRA".
+
+  For mapChart, the columns should be: {id: string, value: number}.
+  For mapChart, sometimes you will ended reaching a field that is a JSON: in this case, you should extract the country.
 
   If your are asked to provide data by country, try to use a mapChart.
 `;
+
 export const expectedOutput = `
 export type SqlChartType = "lineChart" | "pieChart" | "paragraph" | "mapChart";
 
--- packages/backend/src/services/ComputeService.ts
+++ packages/backend/src/services/ComputeService.ts
@@ -1,7 +1,14 @@
-import { ExpectedSqlColumns, SqlChartType } from "@all-for-one/ai";
+import { ExpectedSqlColumns } from "@all-for-one/ai";
 import { clickhouseClient } from "./clickhouse";
-import { mapAiResponse } from "../helpers/mapAiResponse";
-import { PersistentChart } from "../types";
+import {
+  ExpectedOutput,
+  LineChartOutput,
+  MapChartOutput,
+  ParagraphOutput,
+  PersistentChart,
+  PieChartOutput,
+} from "../types";
+import { getPastelColor } from "../helpers/pastelColors";
 
 export class ComputeService {
   async execute(chart: PersistentChart) {
@@ -18,8 +25,88 @@ export class ComputeService {
       data: (await resultSet.json()) as any,
     };
 
-    const formattedResponse = mapAiResponse(chart, rows);
+    const formattedResponse = this.mapAiResponse(chart, rows);
 
     return formattedResponse;
   }
+
+  mapAiResponse = (
+    chart: PersistentChart,
+    response: ExpectedSqlColumns
+  ): ExpectedOutput => {
+    if (!chart.type) {
+      return [];
+    }
+
+    if (response.type === "paragraph") {
+      const paragraphOutput: ParagraphOutput = {
+        id: chart.id,
+        title: chart.title,
+        type: "paragraph",
+        color: getPastelColor(chart.title),
+        data: response?.data[0]?.text ?? "No data found",
+      };
+      return [paragraphOutput];
+    }
+
+    if (response.type === "lineChart") {
+      const groupedByKey = response.data.reduce((acc, item) => {
+        acc[item.groupingKey ?? chart.title] = [
+          ...(acc[item.groupingKey ?? chart.title] || []),
+          {
+            x: item.x,
+            y: item.y,
+          },
+        ];
+        return acc;
+      }, {} as Record<string, Array<{ x: number | string | Date; y: number }>>);
+
+      const lineChartOutput: LineChartOutput = {
+        id: chart.id,
+        title: chart.title,
+        type: "lineChart",
+        data: Object.entries(groupedByKey).map(
+          ([groupingKey, data], index) => ({
+            color: getPastelColor(groupingKey),
+            id: groupingKey,
+            data,
+          })
+        ),
+      };
+
+      return [lineChartOutput];
+    }
+
+    if (response.type === "pieChart") {
+      const pieChartOutput: PieChartOutput = {
+        id: chart.id,
+        title: chart.title,
+        type: "pieChart",
+        data: response.data.map((item) => ({
+          id: item.category,
+          label: item.category,
+          value: Number(item.value),
+          color: getPastelColor(item.category),
+        })),
+      };
+      return [pieChartOutput];
+    }
+
+    if (response.type === "mapChart") {
+      const mapChartOutput: MapChartOutput = {
+        id: chart.id,
+        title: chart.title,
+        type: "mapChart",
+        data: response.data.map((item) => ({
+          id: item.id,
+          value: Number(item.value),
+          color: getPastelColor(item.id),
+        })),
+      };
+
+      return [mapChartOutput];
+    }
+
+    return [];
+  };
 }
