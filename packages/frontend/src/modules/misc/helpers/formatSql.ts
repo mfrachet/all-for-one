35e72b3c636949c035e8066b6f94c085dcbb5729
Marvin Frachet <marvin.frachet@protonmail.com>
2024-10-25 08:46:38 +0200

    fix empty suggestions

--- packages/frontend/src/modules/misc/helpers/formatSql.ts
+++ packages/frontend/src/modules/misc/helpers/formatSql.ts
@@ -1,6 +1,6 @@
 export function formatSQL(query: string) {
   return query
     .replace(/\s+/g, " ") // Remove extra whitespace
-    .replace(/\b(SELECT|FROM|WHERE|ORDER BY)\b/g, "\n$1") // Add newline before keywords
+    .replace(/\b(SELECT|FROM|WHERE|ORDER BY|GROUP BY)\b/g, "\n$1") // Add newline before keywords
     .trim(); // Trim leading/trailing whitespace
 }
