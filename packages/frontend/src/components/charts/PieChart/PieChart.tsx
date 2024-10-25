298518c00e54cf4dfcb76f07c69f1e00798bae6e
Marvin Frachet <marvin.frachet@protonmail.com>
2024-10-25 08:51:35 +0200

    fix empty suggestions

--- packages/frontend/src/components/charts/PieChart/PieChart.tsx
+++ packages/frontend/src/components/charts/PieChart/PieChart.tsx
@@ -11,36 +11,44 @@ export interface PieChartProps {
 
 export const PieChart = ({ data }: PieChartProps) => {
   return (
-    <ResponsivePie
-      data={data}
-      margin={{ top: 40, right: 80, bottom: 40, left: 80 }}
-      innerRadius={0.5}
-      padAngle={0.7}
-      cornerRadius={3}
-      activeOuterRadiusOffset={8}
-      borderWidth={1}
-      colors={{ datum: "data.color" }}
-      borderColor={{
-        from: "color",
-        modifiers: [["darker", 0.2]],
-      }}
-      arcLinkLabelsSkipAngle={10}
-      arcLinkLabelsTextColor="#333333"
-      arcLinkLabelsThickness={2}
-      arcLinkLabelsColor={{ from: "color" }}
-      arcLabelsSkipAngle={10}
-      arcLabelsTextColor={{
-        from: "color",
-        modifiers: [["darker", 2]],
-      }}
-      fill={[
-        {
-          match: {
-            id: "*",
+    <div className="w-full h-full relative">
+      <div className="flex flex-row items-center absolute left-4 bottom-4 gap-2">
+        <div className="text-xs text-gray-500">Total:</div>
+        <div className="text-xs font-bold">
+          {data.reduce((acc, curr) => acc + curr.value, 0)}
+        </div>
+      </div>
+      <ResponsivePie
+        data={data}
+        margin={{ top: 40, right: 80, bottom: 40, left: 80 }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={3}
+        activeOuterRadiusOffset={8}
+        borderWidth={1}
+        colors={{ datum: "data.color" }}
+        borderColor={{
+          from: "color",
+          modifiers: [["darker", 0.2]],
+        }}
+        arcLinkLabelsSkipAngle={10}
+        arcLinkLabelsTextColor="#333333"
+        arcLinkLabelsThickness={2}
+        arcLinkLabelsColor={{ from: "color" }}
+        arcLabelsSkipAngle={10}
+        arcLabelsTextColor={{
+          from: "color",
+          modifiers: [["darker", 2]],
+        }}
+        fill={[
+          {
+            match: {
+              id: "*",
+            },
+            id: "lines",
           },
-          id: "lines",
-        },
-      ]}
-    />
+        ]}
+      />
+    </div>
   );
 };
